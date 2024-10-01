a05dd5bba62438462d2e8d7475d91ec899eb2e1d
Jenny Dang <jenny_dang@kingston.com>
2024-10-01 08:52:23 -0700

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./assets/styles/main.scss";
 function App() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1 className="text-3xl font-bold underline">Hello World</h1>
         </div>
     );
 }
